dfa3cf31f6ad748764166ac5aca40b75bb5a8ff5
Tom Pridham <pridham.tom@gmail.com>
2022-04-30 22:24:46 -0600

    feat: added better error handling and more flexible json type

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,61 @@
 extern crate rocket;
 
 use rocket::data::{Data, ToByteUnit};
-use rocket::fs::TempFile;
-use rocket::futures::StreamExt;
-use rocket::tokio;
+use serde::Serialize;
 
-#[get("/")]
-fn index() -> &'static str {
-    "hey"
+type ArbitraryJson = serde_json::Map<String, serde_json::Value>;
+
+#[derive(Responder, Serialize)]
+struct Err {
+    error: String,
+}
+
+#[derive(Responder)]
+enum UploadResponses {
+    #[response(status = 200, content_type = "json")]
+    RawCsvJson(String),
+    #[response(status = 500, content_type = "json")]
+    UnableToConvertStreamJson(Err),
+    #[response(status = 400, content_type = "json")]
+    BadCsvParseJson(Err),
+    #[response(status = 400, content_type = "json")]
+    FileTooLargeJson(Err),
 }
 
 #[post("/upload-csv", data = "<data>")]
-async fn upload_csv(data: Data<'_>) -> std::io::Result<()> {
+async fn upload_csv(data: Data<'_>) -> UploadResponses {
     let stream = data.open(5.mebibytes());
-    let s = stream.into_string().await?;
+    let s = match stream.into_string().await{
+        Ok(s) => s,
+        Err(_) => return UploadResponses::UnableToConvertStreamJson(Err{error:format!("Unable to open file. Please try again later. If this problem persists, please contact support")})
+    };
 
-    let mut body = s.lines().skip(3).collect::<Vec<_>>();
-    body.pop();
-    let bytes = body.join("\n");
-    println!("b: {bytes:?}");
-    let mut reader = csv_async::AsyncReader::from_reader(bytes.as_bytes());
-    let mut records = reader.records();
-    while let Some(record) = records.next().await {
-        println!("a: {record:?}");
+    if !s.is_complete() {
+        return UploadResponses::FileTooLargeJson(Err { error: format!("This api only supports file uploads up to 5MB. Please reduce the file size and try again") });
     }
-    Ok(())
+
+    // skipping headers
+    let body = s.lines().skip(3).collect::<Vec<_>>();
+    // skipping trailing value
+    let (_, body) = body.split_last().unwrap();
+    let body = body.join("\n");
+
+    let mut reader = csv::Reader::from_reader(body.as_bytes());
+    let records = reader.deserialize::<ArbitraryJson>();
+    let json: Vec<_> = records.flatten().collect();
+
+    let json_str = match serde_json::to_string(&json) {
+        Ok(json_str) => json_str,
+        Err(_) => {
+            return UploadResponses::BadCsvParseJson(Err {
+                error: format!("Unable to parse csv"),
+            })
+        }
+    };
+    UploadResponses::RawCsvJson(json_str)
 }
 
 #[launch]
 fn rocket() -> _ {
-    println!("Hello, world!");
-    rocket::build().mount("/", routes![index, upload_csv])
+    rocket::build().mount("/", routes![upload_csv])
 }
