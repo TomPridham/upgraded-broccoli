15d471eaa6061d0a229fc501afb001532bc96123
Tom Pridham <pridham.tom@gmail.com>
2022-04-30 19:47:40 -0600

    feat: can accept csv uploads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+#[macro_use]
+extern crate rocket;
+
+use rocket::data::{Data, ToByteUnit};
+use rocket::tokio;
+
+#[get("/")]
+fn index() -> &'static str {
+    "hey"
+}
+
+#[post("/upload-csv", data = "<data>")]
+async fn upload_csv(data: Data<'_>) -> std::io::Result<()> {
+    data.open(1024.kibibytes())
+        .stream_to(tokio::io::stdout())
+        .await?;
+    Ok(())
+}
+
+#[launch]
+fn rocket() -> _ {
     println!("Hello, world!");
+    rocket::build().mount("/", routes![index, upload_csv])
 }
